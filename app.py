032020df9abd7f8936ea65634f8103b0c0b554a1
Zhongwl1211 <fses90153@gmail.com>
2023-05-08 15:25:04 +0800

    fix3

--- app.py
+++ app.py
@@ -38,9 +38,18 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r == "抱歉,我不太明白您的意思"
+
+    if msg == 'hi':
+        r = 'hi'
+    if msg == 'hello':
+        r = 'hello'
+    elif msg == '早安':
+        r = '早安,祝您有個開心的一天'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='祝您有個開心的一天'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
